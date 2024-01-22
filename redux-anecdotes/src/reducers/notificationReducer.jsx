9fe886d8f5eb622e9386937c1025c0e79baaba99
anthonydinino <adinino1996@gmail.com>
2024-01-23 04:32:51 +1030

    Exercise 6.12 complete

---
+++ redux-anecdotes/src/reducers/notificationReducer.jsx
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const notificationSlice = createSlice({
+  name: "notification",
+  initialState: "render here notification...",
+  reducers: {
+    setNotification(state, action) {
+      return action.payload.notification;
+    },
+  },
+});
+export const { setNotification } = notificationSlice.actions;
+export default notificationSlice.reducer;
